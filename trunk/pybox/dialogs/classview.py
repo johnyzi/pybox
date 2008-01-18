27a9c2cb42a7e85e875e967a12db2855de80fa47
mindmaster <mindmaster@fdc24ce9-9c43-0410-b362-27590a1e5fac>
2008-01-18 22:23:22 +0000

    Se puede agregar y eliminar items completamente del el treeview de metodos. Se permite seleccion multiple sobre el treeview. El boton Add solamente se activa cuando hay texto en el entry de metodos. Se cambiaron los nombres de los widgets de la solapa de metodos (ver cambios desde Glade)

--- trunk/pybox/dialogs/classview.py
+++ trunk/pybox/dialogs/classview.py
@@ -15,6 +15,7 @@ class ClassView(Window):
         Window.__init__(self, 'class.glade')
         self.view.accept.set_sensitive(False)
         self.view.addattr.set_sensitive(False)
+        self.view.addmethod.set_sensitive(False)
         self.model = model
         self.view.name.set_text(model.name)
         self.view.abstract.set_active(model.abstract)
@@ -25,18 +26,33 @@ class ClassView(Window):
         treeselection_mode = self.view.treeview_attributes.get_selection()
         treeselection_mode.set_mode(gtk.SELECTION_MULTIPLE)
 
+        treeselection_mode = self.view.treeview_methods.get_selection()
+        treeselection_mode.set_mode(gtk.SELECTION_MULTIPLE)
+
         self.load_attributes()
 
     def load_attributes(self):
-        attribute_column = gtk.TreeViewColumn('Atributos',
-                gtk.CellRendererText(), text = 0)
+        # Armamos el treeview de los atributos.
+
+        attribute_column = gtk.TreeViewColumn('Atributos', gtk.CellRendererText(), text = 0)
         self.view.treeview_attributes.append_column(attribute_column)
-        model = gtk.ListStore(str)
+        attribute_model = gtk.ListStore(str)
 
         for item in self.model.variables:
-            model.append([item])
+            attribute_model.append([item])
 
-        self.view.treeview_attributes.set_model(model)
+        self.view.treeview_attributes.set_model(attribute_model)
+
+        # Armamos el treeview de los metodos.
+
+        method_column = gtk.TreeViewColumn('Metodos', gtk.CellRendererText(), text = 0)
+        self.view.treeview_methods.append_column(method_column)
+        method_model= gtk.ListStore(str)
+        
+        for item in self.model.methods:
+            method_model.append([item])
+
+        self.view.treeview_methods.set_model(method_model)
     
     def on_name__changed(self, widget):
         if len(self.view.name.get_text()) > 0:
@@ -49,6 +65,12 @@ class ClassView(Window):
             self.view.addattr.set_sensitive(True)
         else :
             self.view.addattr.set_sensitive(False)
+
+    def on_methodentry__changed(self, widget):
+        if len(self.view.methodentry.get_text()) > 0:
+            self.view.addmethod.set_sensitive(True)
+        else :
+            self.view.addmethod.set_sensitive(False)
 
     def on_accept__clicked(self, widget):
         self.model.name = self.view.name.get_text()
@@ -66,7 +88,20 @@ class ClassView(Window):
         for iter in iters:
             model.remove(iter)
 
+    def on_removemethod__clicked(self, widget):
+        treeview_selection = self.view.treeview_methods.get_selection()
+        model, selected_rows = treeview_selection.get_selected_rows()
+        iters = [model.get_iter(path) for path in selected_rows]
+
+        for iter in iters:
+            model.remove(iter)
+
     def on_addattr__clicked(self, widget):
         model = self.view.treeview_attributes.get_model()
         model.append([self.view.attrentry.get_text()])
         self.view.treeview_attributes.set_model(model)
+
+    def on_addmethod__clicked(self, widget):
+        model = self.view.treeview_methods.get_model()
+        model.append([self.view.methodentry.get_text()])
+        self.view.treeview_methods.set_model(model)
