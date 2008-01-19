fc4482bb0405a0b63a030630d133f0e274c62bbd
hugoruscitti <hugoruscitti@fdc24ce9-9c43-0410-b362-27590a1e5fac>
2008-01-19 23:23:02 +0000

    Agregado el componente Canvas y el menu popup a la ventana principal.

--- pybox/main.py
+++ pybox/main.py
@@ -1,7 +1,7 @@
-
 # -*- encoding: utf-8 -*-
 import gtk
 import gtk.glade
+import goocanvas
 
 from window import Window
 
@@ -13,11 +13,24 @@ class Main(Window):
     def __init__(self):
         Window.__init__(self, 'main.glade')
         self.view.main.show()
-    
+        self._create_canvas()
+
+    def _create_canvas(self):
+        self.view.canvas = goocanvas.Canvas()
+        self.view.eventbox.add(self.view.canvas)
+        self.view.canvas.show()
+        self.view.canvas.connect('event', self.on_event)
+
+    def on_event(self, widget, event):
+        if event.type == gtk.gdk.BUTTON_PRESS and event.button == 3:
+            self.view.popup.popup(None, None, None, event.button, event.time)
+
+    def on_quit_item__activate(self, widget):
+        self.on_main__destroy(widget)
+
+    def on_main__destroy(self, widget):
+        gtk.main_quit()
     
 if __name__ == '__main__':
     main = Main()
     gtk.main()
-
-    
-
