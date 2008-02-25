d9b7922a442bad14f057ce7bd45e44ff5d6a8f39
hugoruscitti <hugoruscitti@fdc24ce9-9c43-0410-b362-27590a1e5fac>
2008-02-25 13:06:43 +0000

    Arreglos en la barra de status inferior.

---
+++ trunk/pybox/status.py
@@ -0,0 +1,53 @@
+# -*- encoding: utf-8 -*-
+import gtk
+import re
+
+class StatusBar(gtk.EventBox):
+    """Muestra los mensajes de estado del programa.
+
+    StatusBar funciona en reemplazo del widget StatusBar de GTK, añadiendo
+    un icono de notificación y varios métodos para facilitar la iteracción
+    con el resto del programa.
+
+    Ejemplo de uso:
+        
+        bar = StatusBar()
+        window.add(bar)
+
+        bar.info('Program started', 'Now you can use this program')
+        gtk.main()
+    """
+
+    def __init__(self):
+        gtk.EventBox.__init__(self)
+        self.ui = gtk.glade.XML('statusbar.glade')
+        self.image = self.ui.get_widget('image')
+        self.status = self.ui.get_widget('status')
+        vbox = self.ui.get_widget("vbox1")
+        vbox.reparent(self)
+        self.set_border_width(3)
+        self.clear()
+        self.show_all()
+
+    def warning(self, message, long=None):
+        self._set_message(gtk.STOCK_DIALOG_WARNING, message, long)
+
+    def error(self, message, long=None):
+        self._set_message(gtk.STOCK_DIALOG_ERROR, message, long)
+
+    def info(self, message, long=None):
+        self._set_message(gtk.STOCK_DIALOG_INFO, message, long)
+
+    def clear(self):
+        self.image.hide()
+        self.status.set_text("")
+
+    def _set_message(self, stock_item, message, long):
+        self.image.show()
+        self.image.set_from_stock(stock_item, gtk.ICON_SIZE_SMALL_TOOLBAR)
+
+        if long:
+            self.status.set_text(" " + message + "...")
+            self.status.set_tooltip_markup(long)
+        else:
+            self.status.set_text(" " + message)
